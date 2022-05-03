317c70cecf41513dd4a1b27d9ed951007b8b073d
Anita Jayana <anje12sth@gmail.com>
2022-05-03 19:10:44 +1000

    managing shared, derived and immutable state, creating and updating shoe slect, display cart items in cart page, update item number, removing item, show number of item in header, storing data in localStorage

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,15 +1,34 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const activeStyle = {
+	color: "red",
+};
 
 export default function Header() {
-  return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <img alt="Carved Rock Fitness" src="/images/logo.png" />
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+	return (
+		<header>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">
+							<img alt="Carved Rock Fitness" src="/images/logo.png" />
+						</Link>
+						<NavLink
+							style={({ isActive }) => (isActive ? activeStyle : undefined)}
+							to="/shoes"
+						>
+							Shoes
+						</NavLink>
+						<NavLink
+							style={({ isActive }) => (isActive ? activeStyle : undefined)}
+							to="/cart"
+						>
+							Cart
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
