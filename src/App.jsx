00b7880a2c553b617606bbfb58977cd366dcf4b1
Anita Jayana <anje12sth@gmail.com>
2022-05-03 22:42:03 +1000

    Implementing Form Validation, add checkout.js, error on empty form submission, tracking touch for validation, checking status of form submission

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Products from "./Products";
 import { Routes, Route } from "react-router-dom";
 import Detail from "./Detail";
 import Cart from "./Cart";
+import Checkout from "./Checkout";
 
 export default function App() {
 	//lifting state
@@ -64,6 +65,7 @@ export default function App() {
 							path="/cart"
 							element={<Cart cart={cart} updateQuantity={updateQuantity} />}
 						/>
+						<Route path="/checkout" element={<Checkout />} />
 					</Routes>
 				</main>
 			</div>
